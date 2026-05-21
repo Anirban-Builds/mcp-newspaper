794475011efc3765b260f0675b7acf35d9913da9
Anirban0011 <bhattandbhatt.999@gmail.com>
2026-05-21 21:02:49 +0530

    upd:3

--- app.py
+++ app.py
@@ -64,5 +64,5 @@ async def handle_ask_news(user_prompt: str):
         )
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
 
